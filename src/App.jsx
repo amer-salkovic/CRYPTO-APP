a85fb603276495da0ca07af37b5a731ce8fa2b4e
AjtanaDjerlek <ajtanadjerlek123@gmail.com>
2024-07-10 23:41:27 +0200

    made Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { Route, Routes } from "react-router"
+
+import { NavBar } from "./Components/NavBar/NavBar"
+import { Home } from "./Components/Home/Home"
+import { Coins } from "./Components/Coins/Coins"
+import { AboutUs } from "./Components/AboutUs/AboutUs"
+import { Favorite } from "./Components/Favorite/Favorite"
+import { LogIn } from "./Components/LogIn/LogIn"
+import { Footer } from "./Components/Footer/Footer"
+
 function App() {
-  return <h1>Hi</h1>
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/favourite" element={<Favorite />} />
+        <Route path="/logIn" element={<LogIn />} />
+      </Routes>
+      <Footer />
+    </>
+  )
 }
 
 export default App
